54394b3198531049666637607335f0a734e73ee1
Henning <grandehg@gmail.com>
2019-10-05 03:24:59 +0200

    Add very basic todo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <input
+          value={input}
+          onChange={({ target }) => setInput(target.value)}
+        />
+        <button
+          onClick={() => {
+            setTodos([...todos, input]);
+          }}
         >
-          Learn React
-        </a>
+          Add todo
+        </button>
+        <ul>
+          {todos.map(todo => (
+            <li
+              onClick={() => {
+                const index = todos.indexOf(todo);
+                setTodos([...todos.slice(0, index), ...todos.slice(index + 1)]);
+              }}
+            >
+              {todo}
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
-}
+};
 
 export default App;
